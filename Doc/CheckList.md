13be2449b2b81eb4ed2ecfe2ed30fd13cb1903c2
Léo Dumesnil-Monbilliard <leohoopa1814@gmail.com>
2026-09-14 17:04:01 +0200

    fix

--- Doc/CheckList.md
+++ Doc/CheckList.md
@@ -1,4 +1,5 @@
 [X] 1.  Ajout d’une série 
 [X] 2.  Liste des séries
 [X] 3.  Détail de la série 
-[] 4.  Détail de la saison 
+[X] 4.  Détail de la saison 
+[O] Mode Utilisateur
--- public/index.php
+++ public/index.php
@@ -10,5 +10,9 @@ include_once __DIR__ . '/header.php';
     <title>WebNovel_Checker</title>
 </head>
 <body>
+    <div>
+        <button class="btn btn-xl btn-warning">Crée un Compte</button>
+        <button class="btn btn-xl btn-info">Se Connecter</button>
+    </div>
 </body>
 </html>
